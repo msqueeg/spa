201a19f820f59275cc3d8964d603f6f6cd6681d4
Michael Miller <millermichael76@gmail.com>
2017-11-25 07:54:41 -0500

    chapter 4: new files

---
+++ js/template.js
@@ -0,0 +1,30 @@
+/*
+ * spa.chat.js
+ * Chat feature module for SPA
+*/
+
+/*jslint		browser	: true,	continue	: true,
+devel	: true,	indent	: 2,	maxerr		: 50,
+newcap	: true,	nomen	: true,	plusplus	: true,
+regexp	: true,	sloppy	: true,	vars		; false,
+white	: true
+*/
+
+/*global $, spa */
+
+spa.chat = (function() {
+	//------------- BEGIN MODULE SCOPE VARIABLES -------------
+	//------------- END MODULE SCOPE VARIABLES ---------------
+
+	//------------- BEGIN UTILTY METHODS ---------------------
+	//------------- END UTILTY METHODS -----------------------
+
+	//------------- BEGIN DOM METHODS ------------------------
+	//------------- END DOM METHODS --------------------------
+
+	//------------- BEGIN EVENT HANDLERS ---------------------
+	//------------- END EVENT HANDLERS -----------------------
+
+	//------------- BEGIN PUBLIC METHODS ---------------------
+	//------------- END PUBLIC METHODS -----------------------
+} ());
